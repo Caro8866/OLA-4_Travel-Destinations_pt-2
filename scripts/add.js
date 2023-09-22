13341f8afd96ab3242f6428ce95875206325900b
Fryderyk Boncler <f.relcnob@gmail.com>
2023-09-22 10:26:58 +0200

    feat: added toaster popups

--- scripts/add.js
+++ scripts/add.js
@@ -117,8 +117,29 @@ document.addEventListener("DOMContentLoaded", () => {
         .then((res) => res.json())
         .then((resJSON) => {
           form.reset();
-          // throw toaster
+          showToaster("positive", `Created ${resJSON.insertedID}`);
+        })
+        .catch((err) => {
+          console.log(err);
+          showToaster("negative", "Something went wrong, try again.");
         });
     }
   });
 });
+
+function showToaster(type, message) {
+  const parent = document.querySelector(".toaster-wrapper");
+  const template = document.querySelector(`#toaster-${type}`);
+  const clone = document.importNode(template.content, true);
+  clone.querySelector("p").textContent = `${message}`;
+  parent.appendChild(clone);
+  setTimeout(() => {
+    parent.querySelector("p:last-of-type").classList.add("show-toast");
+  }, 10);
+  setTimeout(() => {
+    parent.querySelector("p:last-of-type").classList.remove("show-toast");
+  }, 3000);
+  setTimeout(() => {
+    parent.querySelector("p:last-of-type").remove();
+  }, 3800);
+}
