15c378670f8c28b0da17c4c0d643ceb8ea5738d1
Fryderyk Boncler <f.relcnob@gmail.com>
2023-10-03 07:39:46 +0200

    fix: resolved issues with updating images

--- scripts/update.js
+++ scripts/update.js
@@ -1,6 +1,21 @@
-import { validateNonEmpty, validateURL, validateDates, validateImage, validateCountry } from "./utils/validate_helpers.js";
+import {
+  validateNonEmpty,
+  validateURL,
+  validateDates,
+  validateImage,
+  validateCountry,
+} from "./utils/validate_helpers.js";
 import { imageToBase64, compressImage } from "./utils/image_helpers.js";
 import countries from "../utils/countries.js";
+
+function fetchAndPopulate(id) {
+  fetch(`http://localhost:3000/destinations/${id}`)
+    .then((response) => response.json())
+    .then((data) => {
+      populateForm(data);
+    })
+    .catch((error) => console.error("Error:", error));
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector(".update-destination-form");
@@ -16,14 +31,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const urlParams = new URLSearchParams(window.location.search);
   const id = urlParams.get("id");
 
-  fetch(`http://localhost:3000/destinations/${id}`)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      populateForm(data);
-    })
-    .catch((error) => console.error("Error:", error));
-
+  fetchAndPopulate(id);
   let isCountryValid, isNameValid, isLinkValid, isDateValid, isImageValid;
 
   form.addEventListener("submit", async (e) => {
@@ -35,17 +43,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const dateStart = document.querySelector("#arrival-date").value;
     const dateEnd = document.querySelector("#departure-date").value;
     const description = document.querySelector("#description").value;
-    const image = document.querySelector("#image").value;
-
-    const updatedData = {
-      country,
-      name,
-      link,
-      dateStart,
-      dateEnd,
-      description,
-      image,
-    };
+    const image = document.querySelector("#image").files[0];
 
     const isValidCountry = validateCountry(country, countries);
     if (!validateNonEmpty(country)) {
@@ -78,16 +76,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
     if (dateStart && dateEnd && !validateDates(dateStart, dateEnd)) {
       isDateValid = false;
-      showToaster("negative", "Departure date should be after or the same as the arrival date.");
+      showToaster(
+        "negative",
+        "Departure date should be after or the same as the arrival date."
+      );
       return;
     } else {
       isDateValid = true;
     }
 
-    if (image && !validateImage(image)) {
-      showToaster("negative", "Image format should be .jpg, .jpeg or .png");
-      return;
-    }
+    // if (image && !validateImage(image)) {
+    //   showToaster("negative", "Image format should be .jpg, .jpeg or .png");
+    //   return;
+    // }
 
     function base64ToImage(base64) {
       const image = new Image();
@@ -97,7 +98,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let base64;
 
-    if (image) {
+    if (image && image !== document.querySelector("#current-image").src) {
+      console.log(document.querySelector("#image"));
       try {
         const compressedImage = await compressImage(image);
         base64 = await imageToBase64(compressedImage);
@@ -108,9 +110,19 @@ document.addEventListener("DOMContentLoaded", () => {
         return;
       }
     } else {
-      base64 = "";
+      base64 = document.querySelector("#current-image").src;
       isImageValid = true;
     }
+
+    const updatedData = {
+      country: country.trim(),
+      name: name.trim(),
+      link: link.trim(),
+      dateStart: dateStart,
+      dateEnd: dateEnd,
+      description: description.trim(),
+      image: base64,
+    };
 
     let processedInput = {
       country: country.trim(),
@@ -121,7 +133,14 @@ document.addEventListener("DOMContentLoaded", () => {
       description: description.trim(),
       image: base64,
     };
-    if (isCountryValid && isNameValid && isDateValid && isLinkValid && isImageValid) {
+
+    if (
+      isCountryValid &&
+      isNameValid &&
+      isDateValid &&
+      isLinkValid &&
+      isImageValid
+    ) {
       updateDestination(id, updatedData);
     }
   });
@@ -137,8 +156,16 @@ function populateForm(destination) {
   }
   document.querySelector("#title").value = destination.name;
   document.querySelector("#link").value = destination.link;
-  document.querySelector("#arrival-date").value = new Date(destination.dateStart).toISOString().split("T")[0];
-  document.querySelector("#departure-date").value = new Date(destination.dateEnd).toISOString().split("T")[0];
+  document.querySelector("#arrival-date").value = new Date(
+    destination.dateStart
+  )
+    .toISOString()
+    .split("T")[0];
+  document.querySelector("#departure-date").value = new Date(
+    destination.dateEnd
+  )
+    .toISOString()
+    .split("T")[0];
 
   document.querySelector("#description").value = destination.description;
   document.querySelector("#current-image").src = destination.image;
@@ -154,8 +181,8 @@ function updateDestination(id, updatedData) {
   })
     .then((response) => response.json())
     .then((resJSON) => {
-      console.log(resJSON);
       showToaster("positive", `Entry updated successfully`);
+      fetchAndPopulate(id);
     })
     .catch((error) => {
       console.error("Error:", error);
