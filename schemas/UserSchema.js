4ecafadc3ef11bf98cff06e9771fedd79370ac8b
Fryderyk Boncler <f.relcnob@gmail.com>
2023-10-07 16:28:50 +0200

    feat: hooked up signup form to backend

--- schemas/UserSchema.js
+++ schemas/UserSchema.js
@@ -1,20 +1,20 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
-const { Schema } = mongoose;
 
-const UserSchema = new Schema({
-    username: String,
-    password: String,
-  });
+const UserSchema = mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
 
 UserSchema.methods.isValidPassword = function (password) {
   return bcrypt.compare(password, this.password);
-}
+};
 
-UserSchema.pre("save", async function(next) {
-    const hashedPassword = await bcrypt.hash(this.password, 10);
-    this.password = hashedPassword;
-    next();
+UserSchema.pre("save", async function (next) {
+  const hashedPassword = await bcrypt.hash(this.password, 10);
+  this.password = hashedPassword;
+  next();
 });
 
 const User = mongoose.model("User", UserSchema);
--- scripts/authForms.js
+++ scripts/authForms.js
@@ -1,49 +1,79 @@
 document.addEventListener("DOMContentLoaded", function () {
-	const loginButton = document.querySelector(".headline:nth-child(1)");
-	const signupButton = document.querySelector(".headline:nth-child(2)");
+  const loginButton = document.querySelector(".headline:nth-child(1)");
+  const signupButton = document.querySelector(".headline:nth-child(2)");
 
-	const formContainer = document.getElementById("form_container");
-	const loginFormTemplate = document.getElementById("login_form");
-	const signupFormTemplate = document.getElementById("signup_form");
+  const formContainer = document.getElementById("form_container");
+  const loginFormTemplate = document.getElementById("login_form");
+  const signupFormTemplate = document.getElementById("signup_form");
 
-	function loadLoginForm() {
-		formContainer.innerHTML = "";
-		const loginFormClone = document.importNode(loginFormTemplate.content, true);
-		const form = loginFormClone.querySelector("form");
+  function loadLoginForm() {
+    formContainer.innerHTML = "";
+    const loginFormClone = document.importNode(loginFormTemplate.content, true);
+    const form = loginFormClone.querySelector("form");
 
-		loginFormClone
-			.getElementById("login-btn")
-			.addEventListener("click", function (event) {
-				event.preventDefault();
-				form.reset();
-			});
+    loginFormClone
+      .getElementById("login-btn")
+      .addEventListener("click", function (event) {
+        event.preventDefault();
+        form.reset();
+      });
 
-		formContainer.appendChild(loginFormClone);
-		loginButton.classList.add("active");
-		signupButton.classList.remove("active");
-	}
+    formContainer.appendChild(loginFormClone);
+    loginButton.classList.add("active");
+    signupButton.classList.remove("active");
+  }
 
-	loginButton.addEventListener("click", loadLoginForm);
+  loginButton.addEventListener("click", loadLoginForm);
 
-	signupButton.addEventListener("click", function () {
-		formContainer.innerHTML = "";
-		const signupFormClone = document.importNode(
-			signupFormTemplate.content,
-			true
-		);
-		const form = signupFormClone.querySelector("form");
+  signupButton.addEventListener("click", function () {
+    formContainer.innerHTML = "";
+    const signupFormClone = document.importNode(
+      signupFormTemplate.content,
+      true
+    );
+    const form = signupFormClone.querySelector("form");
 
-		signupFormClone
-			.getElementById("signup-btn")
-			.addEventListener("click", function (event) {
-				event.preventDefault();
-				form.reset();
-			});
+    signupFormClone
+      .getElementById("signup-btn")
+      .addEventListener("click", function (event) {
+        event.preventDefault();
 
-		formContainer.appendChild(signupFormClone);
-		signupButton.classList.add("active");
-		loginButton.classList.remove("active");
-	});
+        const email = form.querySelector("#signup-email").value;
+        const username = form.querySelector("#signup-username").value;
+        const password = form.querySelector("#signup-passsword").value;
+        let processedInput;
 
-	loadLoginForm();
+        if (username.length && email.length && password.length) {
+          processedInput = {
+            username: username,
+            password: password,
+            email: email,
+          };
+        } else {
+          processedInput = null;
+        }
+
+        if (processedInput) {
+          fetch("http://localhost:3000/auth/signup", {
+            method: "POST",
+            headers: {
+              "Content-type": "application/json",
+            },
+            body: JSON.stringify(processedInput),
+          }).then((res) => {
+            console.log(res);
+          });
+        } else {
+          console.log("FILL OUT THE FORM");
+        }
+
+        form.reset();
+      });
+
+    formContainer.appendChild(signupFormClone);
+    signupButton.classList.add("active");
+    loginButton.classList.remove("active");
+  });
+
+  loadLoginForm();
 });
